aea617971eccec4b85192595432076a565dc7cad
kokorau <kokorau@gmail.com>
2019-06-13 02:48:50 +0900

    functions

---
+++ src/views/Archives.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>archives</p>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class Archives extends Vue {
+  private photos = [];
+  async mounted() {}
+}
+</script>
+
+<style lang="sass" scoped></style>
