2ec8b752d0aad128e5a166078709b4317d042487
kokorau <kokorau@gmail.com>
2019-06-13 21:50:20 +0900

    google spleadsheetで管理するようにしてみた

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
+import './reset.css'
 import "./font";
 
 Vue.config.productionTip = false;
+
+axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 
 new Vue({
   router,
--- src/views/Archives.vue
+++ src/views/Archives.vue
@@ -1,25 +1,35 @@
 <template>
   <div class="hello">
-    <p>{{ lambdaMsg }}</p>
-    <p><button @click="callLambda">Get Message from Lambda</button></p>
+    <ul class="flyers">
+      <li class="flyer" v-for="flyer of flyers" :key="flyer.date">
+        <img :src="flyer.img" />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import axios from "axios";
+
+const ARCHIVE_ENDPOINT =
+  "https://script.google.com/macros/s/AKfycbw50PfBHw7EBrKhIZUR5u9SG9folyqRJPcB1eViRGj9A9yhoSk/exec";
 
 @Component({})
 export default class Archives extends Vue {
-  private lambdaMsg = "Click to get a message";
-  async mounted() {}
-  callLambda() {
-    fetch("/.netlify/functions/hello")
-      .then(response => response.json())
-      .then(json => {
-        this.lambdaMsg = json.msg;
-      });
+  private flyers = [];
+  async mounted() {
+    const res = await axios.get(ARCHIVE_ENDPOINT);
+    this.flyers = res.data;
   }
 }
 </script>
 
-<style lang="sass" scoped></style>
+<style lang="sass" scoped>
+.flyers
+  display: flex
+  flex-wrap: wrap
+  justify-content: center
+  align-items: flex-start
+  align-content: flex-start
+</style>
